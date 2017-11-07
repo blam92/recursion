375d937c3d3b04c538a4d8556e99de7a60254a98
Santi Iraola <santi@Santis-MacBook-Pro.local>
2017-11-07 15:24:10 -0300

    getElementByClassName working.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var body = document.body;
+  var array = [];
+  var searchClass = function(element, searchedClass) {
+    if(element.classList && element.classList.contains(searchedClass)) {
+    	console.log(element);
+    	array.push(element);
+    }
+
+    var elementChilds = element.childNodes;
+    for(var i = 0; i < elementChilds.length; i++) {
+      searchClass(elementChilds[i], searchedClass);
+    }
+  }
+
+  searchClass(body, className);
+
+  return array;
 };
