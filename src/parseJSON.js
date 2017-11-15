53b16f8a6755ebe35c87bf42feba73b0ebe1e2c9
Santi Iraola <santi@Santis-MacBook-Pro.local>
2017-11-15 12:29:29 -0300

    commented unfinished parseJSON to prevent conflict with the other deliverables.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,139 +3,170 @@
 
 // but you're not, so you'll write it from scratch:
 
-var jsonTest = "{'name':'John','age':28,'isMarried':false,'isFriendly':true,'isAngry':'YES','height':1.90}";
+var jsonTest = "{'name': 'John', 'age': 28, 'isMarried': false,'isFriendly': true,'isAngry': 'YES','height': 1.90}";
 
 var parseJSON = function(json) {
   // your code goes here
-  var tempKey = "";
-  var tempValue = ""; 
-  var values = [];
-  var keys = [];
-  var flagValue = false;
-  var flagArray = false; 
-  var flagObject = false;
+//   var tempKey = "";
+//   var tempValue = ""; 
+//   var values = [];
+//   var keys = [];
+//   var flagValue = false;
+//   var flagArray = false; 
+//   var flagObject = false;
   
-  var parsedString = json.slice(1, json.length - 1);
+//   var parsedString = json.slice(1, json.length - 1);
 
-  var i = 0;
-  /////utility functions /////
-  var nextChar = function() {
-  	i++;
-  }
-  var removeQuotesToString = function(str) {
-  	return str.slice(1, tempKey.length - 1);
-  }
+//   var i = 0;
+//   /////utility functions /////
+//   var nextChar = function() {
+//   	i++;
+//   };
+//   var removeQuotesAndSpacesToString = function(str) {
+//   	var newString = "";
+//   	var hasStringStarted = false;
 
-  var isString = function(str) {
-  	for(var i = 0; i < str.length; i++) {
-		if(str[i] === "'" || str[i] === '"') {
-  			return true;
-  		} else if(str[i] !== " ") {
-  			return false;
-  		}
-  	}
-  	return false;
-  }
+//   	for(var i = 0; i < str.length; i++) {
 
-  var isBool = function(str) {
-  	return !isString(str) && (str.includes("false") || str.includes("true"));
-  }
+//   		if(str[i] === "'" || str[i] === '"') {
+//   			hasStringStarted = !hasStringStarted;
+//   			newString += str[i];
+//   		} else if(hasStringStarted) {
+//   			newString += str[i];
+//   		}
+//   	}
+//   	return newString.slice(1, newString.length - 1);
+//   };
 
-  var parseBoolean = function(str) {
-  	if(str === "false") {
-  		return false;
-  	} else {
-  		return true;
-  	}
-  }
+//   var isString = function(str) {
+//   	for(var i = 0; i < str.length; i++) {
+// 		if(str[i] === "'" || str[i] === '"') {
+//   			return true;
+//   		} else if(str[i] !== " ") {
+//   			return false;
+//   		}
+//   	}
+//   	return false;
+//   };
 
-  var isInt = function(str) {
-  	var digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  	return !isString(str) && str[0] in digits && !str.includes(".");
-  }
-	var isFloat = function(str) {
-		var digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-		return !isString(str) && str[0] in digits && str.includes(".");
-	}
+//   var isBool = function(str) {
+//   	return !isString(str) && (str.includes("false") || str.includes("true"));
+//   };
 
-  // var isValueArrayObjectOrPrimitive = function(valStr) {
-  // 	if(valStr[1] === "[")
-  // }
+//   var parseBoolean = function(str) {
+//   	if(str === "false") {
+//   		return false;
+//   	} else {
+//   		return true;
+//   	}
+//   };
 
-  var transformToCorrectType = function(valStr) {
-    if(isString(valStr)) {
-    	return removeQuotesToString(valStr);
-    } else if(isBool(valStr)) {
-    	return parseBoolean(valStr);
-    } else if(isInt(valStr)) {
-    	return parseInt(valStr);
-    } else if(isFloat(valStr)) {
-    	return parseFloat(valStr);
-    } else {
-    	return undefined;
-    }
-  }
+//   var isInt = function(str) {
+//   	var digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+//   	str = str.split("");
+//   	var hasDigits = str.reduce(function(accumulator, currentValue) {
+//   		if(accumulator === true) {
+//   			return true;
+//   		} else if(currentValue in digits) {
+//   			return true;
+//   		} else {
+//   			return false;
+//   		}
+//   	});
 
-//\\\\\\\utility functions \\\\\\\\
+//   	return !isString(str) && hasDigits && !str.includes(".");
+//   };
+// 	var isFloat = function(str) {
+// 		var digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+// 		str = str.split("");
+//   	var hasDigits = str.reduce(function(accumulator, currentValue) {
+//   		if(accumulator === true) {
+//   			return true;
+//   		} else if(currentValue in digits) {
+//   			return true;
+//   		} else {
+//   			return false;
+//   		}
+//   	});
+// 		return !isString(str) && hasDigits && str.includes(".");
+// 	};
 
-	var getKey = function() {
-	  var tempKey = ""
-	  while(!(flagValue)) {
-	    if(parsedString[i] !== ":") {
-	      tempKey += parsedString[i];
-	    } else {
-	      keys.push(removeQuotesToString(tempKey));
-	      tempKey = "";
-	      flagValue = true;
-	    }
-	    nextChar();
-	  }
-	  getValue();
-	}
+//   // var isValueArrayObjectOrPrimitive = function(valStr) {
+//   // 	if(valStr[1] === "[")
+//   // }
 
-	var getValue = function() {
-	  if(flagValue && !(flagArray || flagObject)) {
-	  	while(flagValue) {
-	  		if(parsedString[i] !== ",") {
-	  			tempValue += parsedString[i];
-	  		} else {
-	  			values.push(tempValue);	
-	  		  tempValue = "";
-	  		  flagValue = false;
-	  		} 
+//   var transformToCorrectType = function(valStr) {
+//     if(isString(valStr)) {
+//     	return removeQuotesAndSpacesToString(valStr);
+//     } else if(isBool(valStr)) {
+//     	valStr.replace(" ", "");
+//     	return parseBoolean(valStr);
+//     } else if(isInt(valStr)) {
+//     	valStr.replace(" ", "");
+//     	return parseInt(valStr);
+//     } else if(isFloat(valStr)) {
+//     	valStr.replace(" ", "");
+//     	return parseFloat(valStr);
+//     } else {
+//     	return undefined;
+//     }
+//   };
 
-	  		if(i >= parsedString.length - 1) {
-	  			values.push(tempValue);	
-	  			flagValue = false;
-	  			return;
-	  	  }
+// //\\\\\\\utility functions \\\\\\\\
 
-	  	  nextChar();
-	    }
-	    getKey();
-	  }
-	}
+// 	var getKey = function() {
+// 	  var tempKey = "";
+// 	  while(!(flagValue)) {
+// 	    if(parsedString[i] !== ":") {
+// 	      tempKey += parsedString[i];
+// 	    } else {
+// 	      keys.push(removeQuotesAndSpacesToString(tempKey));
+// 	      tempKey = "";
+// 	      flagValue = true;
+// 	    }
+// 	    nextChar();
+// 	  }
+// 	  getValue();
+// 	};
 
-	//could be done when pushing keys/values to prevent an extra iteration.
-	var wrapObject = function(vals, ks) {
-		var newObj = {};
+// 	var getValue = function() {
+// 	  if(flagValue && !(flagArray || flagObject)) {
+// 	  	while(flagValue) {
+// 	  		if(parsedString[i] !== ",") {
+// 	  			tempValue += parsedString[i];
+// 	  		} else {
+// 	  			values.push(tempValue);	
+// 	  		  tempValue = "";
+// 	  		  flagValue = false;
+// 	  		} 
 
-		ks.forEach(function(k, index) {
-			newObj[k] = transformToCorrectType(vals[index]);
-		});
+// 	  		if(i >= parsedString.length - 1) {
+// 	  			values.push(tempValue);	
+// 	  			flagValue = false;
+// 	  			return;
+// 	  	  }
 
-		return newObj;
-	}
+// 	  	  nextChar();
+// 	    }
+// 	    getKey();
+// 	  }
+// 	};
 
-  getKey();
+// 	//could be done when pushing keys/values to prevent an extra iteration.
+// 	var wrapObject = function(vals, ks) {
+// 		var newObj = {};
 
-  return wrapObject(values, keys);
+// 		ks.forEach(function(k, index) {
+// 			newObj[k] = transformToCorrectType(vals[index]);
+// 		});
+
+// 		return newObj;
+// 	};
+
+//   getKey();
+
+//   return wrapObject(values, keys);
 };
-
-console.log(parseJSON(jsonTest));
-
-
-
 
 
 
